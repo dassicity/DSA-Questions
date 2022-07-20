9fbee8f05ca1042e5fb552c875361263dcaa92ac
Soumyanil Das <dassic@outlook.com>
2022-07-21 03:46:36 +0530

    TSP Partially done

---
+++ tsp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+
+int min(int A, int B)
+{
+    return (A >= B ? A : B);
+}
+
+int tsp(int size, int startVertex, bool visited[], int graph[][size])
+{
+    int cost = INT_MAX, i, temp;
+
+    for (i = 0; i < size; i++)
+    {
+        if (!visited[i])
+        {
+            visited[i] = true;
+            temp = graph[startVertex][i] + tsp(size, i, visited, graph);
+            if (temp < cost)
+            {
+                cost = temp;
+            }
+        }
+    }
+
+    return cost;
+}
+
+int main()
+{
+    int v, graph[10][10], i, j, cost;
+    bool visited[10];
+
+    printf("Enter the number of vertices: \n");
+    scanf("%d", &v);
+
+    for (i = 0; i < v; i++)
+    {
+        visited[i] = false;
+        for (j = 0; j < v; j++)
+        {
+            printf("Enter value at Graph[%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    cost = tsp(v, 0, visited, graph);
+
+    printf("%d\n", cost);
+
+    return 0;
+}

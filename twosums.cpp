3e37708ce354228831ed684f120051e01a1e1ab5
Soumyanil Das <dassic@outlook.com>
2022-07-27 19:50:39 +0530

    Two sum

--- twosums.cpp
+++ twosums.cpp
@@ -6,19 +6,36 @@ using namespace std;
 vector<int> twoSum(vector<int> &nums, int target)
 {
     int size = nums.size();
+    vector<int> ans;
 
     sort(nums.begin(), nums.end());
 
-    return nums;
+    for (int i = nums.size(); i > 0; i--)
+    {
+        if (nums[i] <= target)
+        {
+            int rem = target - nums[i];
+            for (int j = i; j > 0; j--)
+            {
+                if (nums[j] == rem)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+    }
+
+    return ans;
 }
 
 int main()
 {
-    vector<int> nums{2, 4, 5, 6, 9, 7};
+    vector<int> nums{2, 4, 5, 6};
 
-    twoSum(nums, 9);
+    vector<int> result = twoSum(nums, 9);
 
-    for (auto x : nums)
+    for (auto x : result)
     {
         cout << x << ' ';
     }
